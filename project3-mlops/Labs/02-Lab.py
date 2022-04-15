5ac404c2b195aeafc5c1755a5601a7bca5977c69
ndatta2@u.rochester.edu <ndatta2@u.rochester.edu>
2022-04-15 02:31:58 +0000

    LAB02

--- project3-mlops/Labs/02-Lab.py
+++ project3-mlops/Labs/02-Lab.py
@@ -54,8 +54,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 
 # dictionary containing hyperparameter names and list of values we want to try
-parameters = {'n_estimators': #FILL_IN , 
-              'max_depth': #FILL_IN }
+parameters = {'n_estimators': [100, 1000] , 
+              'max_depth': [5, 10] }
 
 rf = RandomForestRegressor()
 grid_rf_model = GridSearchCV(rf, parameters, cv=3)
@@ -77,19 +77,21 @@ for p in parameters:
 # TODO
 from sklearn.metrics import mean_squared_error
 
-with mlflow.start_run(run_name= FILL_IN) as run:
+with mlflow.start_run(run_name= "RF-Grid-Search") as run:
   # Create predictions of X_test using best model
-  # FILL_IN
-  
+  predictions = best_rf.predict(X_test)
+    
+
   # Log model with name
-  # FILL_IN
-  
+  mlflow.sklearn.log_model(rf, "grid-random-forest-model")
   # Log params
-  # FILL_IN
+  mlflow.log_params(parameters)
   
   # Create and log MSE metrics using predictions of X_test and its actual value y_test
-  # FILL_IN
-  
+  mse = mean_squared_error(y_test, predictions)
+  mlflow.log_metric("mse", mse)
+  experimentID = run.info.experiment_id
+
   runID = run.info.run_uuid
   print("Inside MLflow Run with id {}".format(runID))
 
@@ -110,7 +112,13 @@ with mlflow.start_run(run_name= FILL_IN) as run:
 # COMMAND ----------
 
 # TODO
-model = < FILL_IN >
+from  mlflow.tracking import MlflowClient
+
+client = MlflowClient()
+runs = client.search_runs(experimentID, order_by=["attributes.start_time desc"], max_results=1)
+artifactURI = 'runs:/'+runs[0].info.run_id+"/grid-random-forest-model"
+model = mlflow.sklearn.load_model(artifactURI)
+
 
 # COMMAND ----------
 
